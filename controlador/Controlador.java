05b14b1e552937f33734cf290bf2e1ae66d8db6f
Anderson Andrés Fonseca Álvarez <102609948+AndersonFonseca1704@users.noreply.github.com>
2022-11-23 19:09:31 -0500

    Add files via upload

---
+++ controlador/Controlador.java
@@ -0,0 +1,60 @@
+package controlador;
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import modelo.Biblioteca;
+import vista.VentanaPrincipal;
+
+public class Controlador implements ActionListener
+{
+    //------------------
+    //Atributos
+    //------------------
+    private VentanaPrincipal venPrin;
+    private Biblioteca biblioteca;
+
+    //------------------
+    //Metodos
+    //------------------
+
+    //Constructor
+    public Controlador(VentanaPrincipal pVenPrin, Biblioteca pBiblio)
+    {
+        this.venPrin = pVenPrin;
+        this.biblioteca= pBiblio;
+        this.venPrin.miPanelOperaciones.agregarOyentesBotones(this);
+        this.venPrin.miPanelResultados.mostrarResultado("");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) 
+    {
+        String comando = ae.getActionCommand();
+
+        //Abrir ventana Agregar vendedor
+        if(comando.equals("AgregarColeccion"))
+        {
+            venPrin.crearDialogoAgregarColeccion();
+            this.venPrin.miColeccion.agregarOyenteBoton(this);
+        }
+        if(comando.equals("agregarLibro"))
+        {
+            venPrin.crearDialogoAgregarLibro();
+            this.venPrin.miLibro.agregarOyenteBoton(this);
+        }
+        if(comando.equals("agregar1"))
+        {
+
+        }
+        if(comando.equals("agregar2"))
+        {
+            
+        }
+        if(comando.equals("salir"))
+        {
+            System.exit(0);
+        }  
+    }
+}
